0dee546d87ff09f5ca003aef7e79d05e7036e817
forrestmaners446 <70077153+forrestmaners446@users.noreply.github.com>
2020-12-11 10:39:49 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,6 @@
- $(document).ready(function () {
-            $(document).on("scroll", onScroll);
-            //smoothscroll
-            $('.navbar-nav li a[href^="#"]').on('click', function (e) {
-                e.preventDefault();
-                $(document).off("scroll");
-                $('.navbar-nav li a').each(function () {
-                    $(this).removeClass('active');
-                })
-                $(this).addClass('active');
-                var target = this.hash
-                    , menu = target;
-                target = $(target);
-                $('html, body').stop().animate({
-                    'scrollTop': target.offset().top + 2
-                }, 1000, 'swing', function () {
-                    window.location.hash = target;
-                    $(document).on("scroll", onScroll);
-                });
-            });
-        });
+/**
+ * index.js
+ * - All our useful JS goes here, awesome!
+ */
 
-        function onScroll(event) {
-            var scrollPos = $(document).scrollTop();
-            $('.navbar-nav li a').each(function () {
-                var currLink = $(this);
-                var refElement = $(currLink.attr("href"));
-                if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-                    $('.navbar-nav li a').removeClass("active");
-                    currLink.addClass("active");
-                }
-                else {
-                    currLink.removeClass("active");
-                }
-            });
-        }
+console.log("JavaScript is amazing!");
